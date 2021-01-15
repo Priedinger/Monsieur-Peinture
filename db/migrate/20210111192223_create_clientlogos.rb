e806254d501497ab51f7c24a7ed3a04c0598b612
pierre riedinger <riedingerpierre@gmail.com>
2021-01-15 13:19:18 +0100

    added published booleans

--- db/migrate/20210111192223_create_clientlogos.rb
+++ db/migrate/20210111192223_create_clientlogos.rb
@@ -2,6 +2,7 @@ class CreateClientlogos < ActiveRecord::Migration[6.0]
   def change
     create_table :clientlogos do |t|
       t.string :name
+      t.boolean :published, default: false
 
       t.timestamps
     end

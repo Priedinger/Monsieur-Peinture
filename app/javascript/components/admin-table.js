46e7dabc00bed25a4aeb92bd729bcd9d97130cc7
pierre riedinger <riedingerpierre@gmail.com>
2021-01-16 19:53:27 +0100

    toggle admin

--- app/javascript/components/admin-table.js
+++ app/javascript/components/admin-table.js
@@ -1,44 +1,15 @@
-
-  let adminBanner = document.getElementById('admin-banners')
-  let adminCounter = document.getElementById('admin-counter')
-  let adminLogos = document.getElementById('admin-logos')
-  let adminVerbatims = document.getElementById('admin-verbatims')
-  let adminRealisations = document.getElementById('admin-realisations')
-  let adminArticles = document.getElementById('admin-articles')
-
+const adminPage = document.querySelector('.admin-page')
 const initAdminTableToggle = () => {
- if(adminBanner || adminCounter || adminLogos || adminVerbatims || adminRealisations || adminRealisations || adminArticles) {
+  if(adminPage) {
+    function unHiddeTable(e) {
+      const table = document.getElementById(e.target.id).nextElementSibling
+      table.classList.toggle("hidden-admin-table");
+    }
 
-  adminBanner.addEventListener("click", () => {
-    let adminTableBanner = document.getElementById('admin-table-banners')
-    adminTableBanner.classList.toggle("hidden");
-  })
-  adminCounter.addEventListener("click", () => {
-    let adminTableCounter = document.getElementById('admin-table-counter')
-    adminTableCounter.classList.toggle("hidden");
-  })
-
-  adminLogos.addEventListener("click", () => {
-    let adminTableLogos = document.getElementById('admin-table-logos')
-    adminTableLogos.classList.toggle("hidden");
-  })
-
-  adminVerbatims.addEventListener("click", () => {
-    let adminTableVerbatims = document.getElementById('admin-table-verbatims')
-    adminTableVerbatims.classList.toggle("hidden");
-  })
-
-  adminRealisations.addEventListener("click", () => {
-    let adminTableRealisations = document.getElementById('admin-table-realisations')
-    adminTableRealisations.classList.toggle("hidden");
-  })
-
-  adminArticles.addEventListener("click", () => {
-    let adminTableArticles = document.getElementById('admin-table-articles')
-    adminTableArticles.classList.toggle("hidden");
-  })
+    const titles = document.querySelectorAll('h3');
+    titles.forEach((title) => {
+      title.addEventListener('click', unHiddeTable);
+    });
+  };
 };
-
-};
-
 export { initAdminTableToggle };

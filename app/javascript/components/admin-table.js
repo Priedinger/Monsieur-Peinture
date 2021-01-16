6b4d97a618774c3cc7d9b63bb14d8882a4c2dbe4
pierre riedinger <riedingerpierre@gmail.com>
2021-01-16 18:09:35 +0100

    toggle

--- app/javascript/components/admin-table.js
+++ app/javascript/components/admin-table.js
@@ -8,18 +8,37 @@
 
 const initAdminTableToggle = () => {
  if(adminBanner || adminCounter || adminLogos || adminVerbatims || adminRealisations || adminRealisations || adminArticles) {
-  function logItem(e) {
-    const item = document.querySelector(`[data-id=${e.target.id}]`);
-    item.toggleAttribute('hidden');
-  }
 
-  const tables = document.querySelector('[id^="admin-table-"]').id;
+  adminBanner.addEventListener("click", () => {
+    let adminTableBanner = document.getElementById('admin-table-banners')
+    adminTableBanner.classList.toggle("hidden");
+  })
+  adminCounter.addEventListener("click", () => {
+    let adminTableCounter = document.getElementById('admin-table-counter')
+    adminTableCounter.classList.toggle("hidden");
+  })
 
-  tables.forEach((table) => {
-    table.addEventListener('toggle', logItem);
-  });
+  adminLogos.addEventListener("click", () => {
+    let adminTableLogos = document.getElementById('admin-table-logos')
+    adminTableLogos.classList.toggle("hidden");
+  })
+
+  adminVerbatims.addEventListener("click", () => {
+    let adminTableVerbatims = document.getElementById('admin-table-verbatims')
+    adminTableVerbatims.classList.toggle("hidden");
+  })
+
+  adminRealisations.addEventListener("click", () => {
+    let adminTableRealisations = document.getElementById('admin-table-realisations')
+    adminTableRealisations.classList.toggle("hidden");
+  })
+
+  adminArticles.addEventListener("click", () => {
+    let adminTableArticles = document.getElementById('admin-table-articles')
+    adminTableArticles.classList.toggle("hidden");
+  })
 };
-};
 
+};
 
 export { initAdminTableToggle };

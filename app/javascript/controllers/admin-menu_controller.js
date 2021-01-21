253a7096cb47e478133fae96819cea0469c68d2e
Victor Branger <victor.branger@gmail.com>
2021-01-21 21:27:24 +0100

    admin new version

---
+++ app/javascript/controllers/admin-menu_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ 'content', 'table']
+
+  connect() {
+    console.log(this.tableTarget)
+    this.tableTarget.classList.remove('hidden-admin-table')
+  }
+
+  display() {
+    event.preventDefault();
+    console.log(event.target)
+    this.tableTargets.forEach(table => {
+      if (!table.classList.contains('hidden-admin-table')) {
+        table.classList.add('hidden-admin-table');
+      }
+    })
+    const table = document.getElementById(event.target.dataset.id)
+    table.classList.remove('hidden-admin-table');
+  }
+
+  // test qui ne fonctionne pas mais que je veux bien garder pour y retravailler plus tard si possible !
+  // show() {
+  //   event.preventDefault();
+  //   fetch('/banners', { headers: { accept: "application/json" } })
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       this.contentTarget.innerHTML = `<%= j render partial: 'shared/banners_row', locals: {all_banners: ${data}} %>`
+  //     });
+
+  // }
+
+}

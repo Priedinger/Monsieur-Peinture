76aa2fc88bff8926fd3d92a360a4b1e687ae0c65
Victor Branger <victor.branger@gmail.com>
2021-01-25 21:48:56 +0100

    position diplayed

--- app/controllers/clientlogos_controller.rb
+++ app/controllers/clientlogos_controller.rb
@@ -3,6 +3,10 @@ class ClientlogosController < ApplicationController
 
   def index
     @clientlogos = Clientlogo.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { clientlogos: @clientlogos } }
+    end
   end
 
   def show
